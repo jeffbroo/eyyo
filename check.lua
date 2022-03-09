6c6239a0834b974381707dd67c0fd957a6a8e045
jeffbroo <88398078+jeffbroo@users.noreply.github.com>
2022-03-09 17:11:12 +0300

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,1 +1,13 @@
+local username_true = {}
+local username = globals.get_username()
+username_true['w0psy'] = true
 
+local function sendtrue()
+    if username_true[username] == true then
+      return true
+    else
+      return false
+    end
+end
+
+return sendtrue()
